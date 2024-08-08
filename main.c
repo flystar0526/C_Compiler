eb3b892ace7e6474367781c9bb9049cf2a43472f
flystar0526 <flowermetoer@gmail.com>
2024-08-08 21:06:20 +0800

    add tokenizer

--- main.c
+++ main.c
@@ -1,5 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdarg.h>
+
+typedef enum {
+    TK_OPERATOR,
+    TK_NUM,
+    TK_EOF,
+} TokenType;
+
+typedef struct Token {
+    TokenType type;
+    struct Token *next;
+    int value;
+    char *str;    
+} Token;
+
+Token *token;
+
+void PrintError(char *format, ...) {
+    va_list list;
+    va_start(list, format);
+    vfprintf(stderr, format, list);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+Token * CreateToken(TokenType type, Token *cur, char *str) {
+    Token *token = calloc(1, sizeof(Token));
+    token->type = type;
+    token->str = str;
+    cur->next = token;
+
+    return token;
+}
+
+Token * Tokenize(char* p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p ++;
+        } else if (*p == '+' || *p == '-') {
+            cur = CreateToken(TK_OPERATOR, cur, p ++);
+        } else if (isdigit(*p)) {
+            cur = CreateToken(TK_NUM, cur, p);
+            cur->value = strtol(p, &p, 10);
+        } else {
+            PrintError("Tokenize failed");
+        }
+    }
+
+    CreateToken(TK_EOF, cur, p);
+    return head.next;
+}
+
+int ExpectNumber() {
+    if (token->type != TK_NUM) {
+        PrintError("Not Number");
+    }
+
+    int value = token->value;
+    token = token->next;
+
+    return value;
+}
+
+bool ExpectSymbol(char operator) {
+    if (token->type != TK_OPERATOR || token->str[0] != operator) {
+        return false;
+    }
+
+    token = token->next;
+    return true;
+}
+
+bool AtEof() {
+    return token->type == TK_EOF;
+}
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -7,23 +89,21 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    char *p = argv[1];
+    token = Tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-    while (*p) {
-        if (*p == '+') {
-            p ++;
-            printf("  add rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        } else if (*p == '-') {
-            p ++;
-            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+    printf("  mov rax, %d\n", ExpectNumber());
+
+    while (!AtEof()) {
+        if (ExpectSymbol('+')) {
+            printf("  add rax, %d\n", ExpectNumber());
+        } else if (ExpectSymbol('-')) {
+            printf("  sub rax, %d\n", ExpectNumber());
         } else {
-            fprintf(stderr, "Unknown number: '%c'\n", *p);
+            PrintError("Unknown symbol");
         }
     }
 
